3f7be2ef3f830ff60373a4f347ace08950a87d5b
Zakaria Bennane <86924575+ZakariaBennane99@users.noreply.github.com>
2023-02-26 21:21:39 +0200

    Update sitemap.js

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -10,9 +10,55 @@ export default function handler(req, res) {
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
     <url>
-      <loc>http://www.example.com/foo.html</loc>
-      <lastmod>2021-01-01</lastmod>
-    </url>
+      <loc>https://zakariabennane.com/blog</loc>
+      <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+      <priority>1</priority>
+     </url>
+     <url>
+      <loc>https://zakariabennane.com/projects</loc>
+      <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+      <priority>1</priority>
+     </url>
+     <url>
+      <loc>https://zakariabennane.com/about</loc>
+      <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+      <priority>1</priority>
+     </url>
+     <url>
+      <loc>https://zakariabennane.com/blog/seo-with-react</loc>
+      <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+      <priority>1</priority>
+     </url>
+     <url>
+      <loc>https://zakariabennane.com/blog/social-sharing-and-virality</loc>
+      <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+      <priority>1</priority>
+     </url>
+     <url>
+      <loc>https://zakariabennane.com/blog/summary-of-positioning</loc>
+      <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+      <priority>1</priority>
+     </url>
+     <url>
+      <loc>https://zakariabennane.com/blog/deploy-a-nodejs-application-with-nginx-on-your-vps-server</loc>
+        <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+        <priority>1</priority>
+      </url>
+      <url>
+        <loc>https://zakariabennane.com/blog/indie-makers:-how-to-dramatically-increase-your-conversion-rate</loc>
+        <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+        <priority>1</priority>
+      </url>
+      <url>
+        <loc>https://zakariabennane.com/blog/my-web-development-journey</loc>
+        <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+        <priority>1</priority>
+      </url>
+      <url>
+        <loc>https://zakariabennane.com/projects/proderto</loc>
+        <lastmod>2023-02-26T19:17:16+00:00</lastmod>
+        <priority>1</priority>
+      </url>
     </urlset>`
 
   res.end(xml)
